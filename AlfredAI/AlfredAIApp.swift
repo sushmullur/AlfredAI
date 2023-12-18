307d03c4b45bd6b442e39329c49874dba346130a
sushmullur <mullur.sushruth@gmail.com>
2023-12-17 19:39:22 -0800

    Initial Commit

---
+++ AlfredAI/AlfredAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlfredAIApp.swift
+//  AlfredAI
+//
+//  Created by Sush Mullur on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct AlfredAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
