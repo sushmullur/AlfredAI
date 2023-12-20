b7f4b970721ba48cee20877d868b2af282029287
sushmullur <mullur.sushruth@gmail.com>
2023-12-20 11:24:08 -0800

    History page added

--- AlfredAI/HomeScreen.swift
+++ AlfredAI/HomeScreen.swift
@@ -1,28 +1,23 @@
-//
-//  HomeScreen.swift
-//  AlfredAI
-//
-//  Created by Sush Mullur on 12/17/23.
-//
-
 import SwiftUI
 
 struct HomeScreen: View {
     @State private var isSpeaking = false
 
     var body: some View {
-        ZStack {
-            Color("AlfredDarkGray")
-                .edgesIgnoringSafeArea(.all)
+        NavigationView {
+            ZStack {
+                Color("AlfredDarkGray")
+                    .edgesIgnoringSafeArea(.all)
 
-            VStack {
-                TopIcons()
-                Spacer(minLength: 20)
-                AlfredView(isSpeaking: $isSpeaking)
-                Spacer()
-                TextBox(text: "Hello, I'm Alfred. How may I be of assistance?")
-                Spacer()
-                CircleIcon(iconName: "ReplayIcon", iconSize: 50)
+                VStack {
+                    TopIcons()
+                    Spacer(minLength: 20)
+                    AlfredView(isSpeaking: $isSpeaking)
+                    Spacer()
+                    TextBox(text: "Hello, I'm Alfred. How may I be of assistance?")
+                    Spacer()
+                    CircleIcon(iconName: "ReplayIcon", iconSize: 50)
+                }
             }
         }
     }
@@ -38,8 +33,12 @@ struct HomeScreen_Previews: PreviewProvider {
 struct TopIcons: View {
     var body: some View {
         HStack {
-            CircleIcon(iconName: "HistoryIcon", iconSize: 50)
+            NavigationLink(destination: HistoryPage()) {
+                CircleIcon(iconName: "HistoryIcon", iconSize: 50)
+            }
+            
             Spacer()
+
             CircleIcon(iconName: "SettingsIcon", iconSize: 50)
         }
         .padding(.horizontal)
---
+++ AlfredAI/Query.swift
@@ -0,0 +1,6 @@
+struct Query: Identifiable {
+    var id: String  // query_id
+    var time: String  // query_time
+    var request: String  // query_request
+    var response: String  // query_response
+}
